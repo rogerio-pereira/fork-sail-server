e987ce7bb045e7d01de4a9d4ad2d5eb71a0e14bb
Rogerio Pereira <hi@rogeriopereira.dev>
2025-05-02 19:22:33 -0400

    Add title tag app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Laravel Sailor</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         @vite('resources/css/app.css')
